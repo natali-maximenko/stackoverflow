2e9993bf27eb6dcefacc7508b564e4daf71c7dd8
Natali Maximenko <natali.maximenko@gmail.com>
2019-07-26 16:17:39 +0300

    Move votable model spec to shared examples

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Answer, type: :model do
 
   it { should accept_nested_attributes_for :links }
 
+  it_behaves_like 'votable'
+
   it 'have ne attached file' do
     expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)  
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Question, type: :model do
 
   it { should accept_nested_attributes_for :links }
 
+  it_behaves_like 'votable'
+
   it 'have ne attached file' do
     expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)  
   end
