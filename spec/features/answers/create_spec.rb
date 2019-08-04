fa9796a9147ef22f4cfb06b6743bc4e6902a5e32
Natali Maximenko <natali.maximenko@gmail.com>
2019-08-04 22:25:35 +0300

    Publish answer

--- spec/features/answers/create_spec.rb
+++ spec/features/answers/create_spec.rb
@@ -1,6 +1,8 @@
 feature "Create answer", type: :feature do
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
+  given(:girl) { create(:user) }
+  given(:question2) { create(:question, user: girl) }
 
   describe 'Authenticated user' do
     background do
@@ -37,5 +39,51 @@ feature "Create answer", type: :feature do
     
     expect(page).to_not have_content 'Create Answer'
     expect(page).to_not have_button 'Reply'
+  end
+
+  describe 'Multiple sessions' do
+    scenario 'answer appears on girl page', js: true do
+      Capybara.using_session('girl') do
+        sign_in(girl)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('girl') do
+        fill_in 'Your answer', with: 'Answer'
+        click_on 'Reply'
+
+        expect(current_path).to eq question_path(question)
+        within '.answers' do
+          expect(page).to have_content 'Answer'
+        end
+      end
+
+      Capybara.using_session('user') do
+        within '.answers' do
+          expect(page).to have_content 'Answer'
+          expect(page).to have_content 'Rating:'
+          expect(page).to have_link 'Up!'
+          expect(page).to have_link 'Down'
+          expect(page).to have_link 'Make best'
+        end
+      end
+
+      Capybara.using_session('guest') do
+        within '.answers' do
+          expect(page).to have_content 'Answer'
+          expect(page).to have_link 'Like!'
+          expect(page).to have_link 'Dislike...'
+        end
+      end
+    end
   end
 end
